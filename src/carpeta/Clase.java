1a144f9cd9786cfddafbcce42e79fb09b39f1097
Arrocero <f4r3ver@gmail.com>
2024-01-03 02:01:47 -0500

    Mi aporte es el agregar otro sout + tab en la clase Clase.java

--- src/carpeta/Clase.java
+++ src/carpeta/Clase.java
@@ -4,5 +4,6 @@ package carpeta;
 public class Clase {
     public static void main(String[] args) {
         System.out.println("Hola Mundo :)");
+        System.out.println("Agregamos más texto");
     }
 }
